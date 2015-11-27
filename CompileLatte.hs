fd572fb819725f3b639130cd0719874bfc6b55f4
Łukasz Piesiewicz <wookesh@gmail.com>
2015-11-27 11:06:27 +0100

    data types

--- CompileLatte.hs
+++ CompileLatte.hs
@@ -1,5 +1,17 @@
 module CompileLatte where
 
+import Data.Map as M
+import Data.Set as S
+
+type Reg = Int
+
+type VEnv = M.Map Ident Reg
+type FEnv = M.Map Ident [Arg]
+type CEnv = M.Map Ident [Arg] [FEnv]
+
+data Store = ST (VEnv, FEnv, CEnv) deriving (Eq, Ord, Show)
+
 
 class Compile a where
 	compile a -> StateT Store IO b
+
--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 bnfc -m Latte.cf
+make
